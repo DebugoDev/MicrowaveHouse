b2e8c3ed8f523a3c96e2b035c5fad93af5b22056
Mavi <mariapimenta2607@gmail.com>
2025-05-31 00:05:02 -0300

    fix: SEO on all pages

--- microwavehouse/src/pages/HomePage/Home.tsx
+++ microwavehouse/src/pages/HomePage/Home.tsx
@@ -1,15 +1,18 @@
-import { useEffect } from "react";
 import { Helmet } from "react-helmet-async";
 
 const Home = () => {
 
-    useEffect(() => {
-        document.title = 'Casa do Micro-Ondas | Home';
-      }, []);
-
     return (
         <>
-            <Helmet></Helmet>
+            <Helmet>
+                <title>Home | Casa do Micro-Ondas</title>
+                <meta name="description" content="Entre em contato com a Casa do Micro-Ondas para consertos e serviços." />
+
+                <meta property="og:title" content="Fale Conosco - Casa do Micro-Ondas" />
+                <meta property="og:description" content="Estamos prontos para te atender. Veja nossos canais de contato." />
+                <meta property="og:url" content="https://www.casadomicroondas.com.br/" />
+                <meta property="og:type" content="website" />
+            </Helmet>
         </>
     );
 };
